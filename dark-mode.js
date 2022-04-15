967470d9716c274a9c0c8be001ce38eac41fa1e8
gabriel antunes <gabriel.antunes@acad.pucrs.br>
2022-04-14 23:57:29 -0300

    Adicionado footer, ajustado algumas partes da responsividade e cores adequadas para o tema escuro.

--- dark-mode.js
+++ dark-mode.js
@@ -1,14 +1,17 @@
 const btnDark = document.querySelector('.btn-dark');
 const rootDark = document.querySelector('html');
+const imgPrincipal = document.querySelector('.foto-principal')
 let darkOn = false;
 btnDark.addEventListener('click',()=>{
     if (darkOn === false){
         btnDark.classList.add('ativo');
         rootDark.classList.add('dark-mode');
+        imgPrincipal.classList.add('imgDarkMode');
         darkOn = true
     }else{
         btnDark.classList.remove('ativo');
         rootDark.classList.remove('dark-mode');
+        imgPrincipal.classList.remove('imgDarkMode');
         darkOn = false
     }
 })

1b427d08071e004ade51971df2d5d9bd5b5c58a3
gabriel antunes <gabriel.antunes@acad.pucrs.br>
2022-04-17 17:05:51 -0300

    terminado seção FAQ e ajustado responsividade de todo o site

--- btn-faqs.js
+++ btn-faqs.js
@@ -27,3 +27,45 @@ btnFaq2.addEventListener('click',()=>{
         btnFaqOn2 = false
     }
 })
+const btnFaq3 = document.querySelector('.btn-faq3');
+const faqRes3 = document.querySelector('.resp-faq3')
+let btnFaqOn3 = false;
+btnFaq3.addEventListener('click',()=>{
+    if (btnFaqOn3 === false){
+        btnFaq3.classList.add('faq-on');
+        faqRes3.classList.add('open-resposta');
+        btnFaqOn3 = true
+    }else{
+        btnFaq3.classList.remove('faq-on');
+        faqRes3.classList.remove('open-resposta');
+        btnFaqOn3 = false
+    }
+})
+const btnFaq4 = document.querySelector('.btn-faq4');
+const faqRes4 = document.querySelector('.resp-faq4')
+let btnFaqOn4 = false;
+btnFaq4.addEventListener('click',()=>{
+    if (btnFaqOn4 === false){
+        btnFaq4.classList.add('faq-on');
+        faqRes4.classList.add('open-resposta');
+        btnFaqOn4 = true
+    }else{
+        btnFaq4.classList.remove('faq-on');
+        faqRes4.classList.remove('open-resposta');
+        btnFaqOn4 = false
+    }
+})
+const btnFaq5 = document.querySelector('.btn-faq5');
+const faqRes5 = document.querySelector('.resp-faq5')
+let btnFaqOn5 = false;
+btnFaq5.addEventListener('click',()=>{
+    if (btnFaqOn5 === false){
+        btnFaq5.classList.add('faq-on');
+        faqRes5.classList.add('open-resposta');
+        btnFaqOn5 = true
+    }else{
+        btnFaq5.classList.remove('faq-on');
+        faqRes5.classList.remove('open-resposta');
+        btnFaqOn5 = false
+    }
+})
